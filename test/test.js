6df968480750f59f70fb4d5026f6a4a253097681
Yosuke Furukawa <furukawa.yosuke@dena.jp>
2014-12-15 19:24:35 +0900

    init

---
+++ test/test.js
@@ -0,0 +1,14 @@
+let assert = require("power-assert")
+let Person = require("../person")
+
+describe("Person", ()=>{
+  let name = "Alice"
+  let age = 4
+  let alice = new Person(name, age)
+  it("alice get age", ()=>{
+    assert.equal(alice.getAge(), age)
+  })
+  it("alice greet", ()=>{
+    assert.equal(alice.greet(), `Hello! I am ${name}. My age is ${age}`)
+  })
+})
